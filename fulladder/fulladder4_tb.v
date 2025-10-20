99ffefcc10c12c0a62a9b10e5d1aa7a66a81e7e1
Urko Lopez <urkolopezlopez@gmail.com>
2025-10-20 15:56:25 +0200

    fix: initial cin value for TB

--- fulladder/fulladder4_tb.v
+++ fulladder/fulladder4_tb.v
@@ -3,23 +3,28 @@
 module fulladder4_tb ();
     reg [3:0] a;
     reg [3:0] b;
-    wire cin;
+    wire cin = 0;
 
     wire [3:0] s;
     wire cout;
 
     fulladder4 uut(.a(a), .b(b), .cin(cin), .s(s), .cout4(cout));
-
     initial begin
-        $monitor("%b + %b = %b", a, b, s);
+        $monitor("%b + %b = %b; Carry = %b", a, b, s, cout);
         a = 4'b0001;
         b = 4'b0001;
         #2000
         a = 4'b0011;
         b = 4'b0001;
         #2000
-        a = 4'b0011;
+        a = 4'b0001;
         b = 4'b0111;
+	#2000
+	a = 4'b1000;
+	b = 4'b0111;
+	#2000
+	a = 4'b1000;
+	b = 4'b1111;
     end
 endmodule
 
